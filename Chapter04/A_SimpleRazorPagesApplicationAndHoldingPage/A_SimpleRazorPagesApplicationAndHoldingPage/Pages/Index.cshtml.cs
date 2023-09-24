2fc5887d7433cf286917a0ff66f2e68d053848be
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-09-25 00:42:54 +0900

    Chapter 04: Add A_SimpleRazorPagesApplicationAndHoldingPage solution

---
+++ Chapter04/A_SimpleRazorPagesApplicationAndHoldingPage/A_SimpleRazorPagesApplicationAndHoldingPage/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace A_SimpleRazorPagesApplicationAndHoldingPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
