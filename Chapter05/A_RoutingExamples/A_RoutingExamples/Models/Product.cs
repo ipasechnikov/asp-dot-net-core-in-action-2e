0bcf93852e47d140e3416e876fc3f00b459edd46
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-09-29 21:57:04 +0900

    Chapter 05: Add product model and service to A_RoutingExamples solution

---
+++ Chapter05/A_RoutingExamples/A_RoutingExamples/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace A_RoutingExamples.Models
+{
+	public class Product
+	{
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+
+		public Product(string name, decimal price)
+		{
+			Name = name;
+			Price = price;
+		}
+	}
+}
---
+++ Chapter05/A_RoutingExamples/A_RoutingExamples/Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using A_RoutingExamples.Models;
+
+namespace A_RoutingExamples.Services
+{
+	public class ProductService
+	{
+		private static readonly IDictionary<string, Product> _allProducts =
+			new Dictionary<string, Product>
+			{
+				{ "big-widget", new Product("Big Widget", 123) },
+				{ "super-fancy-widget", new Product("Super fancy widget", 456) },
+			};
+
+		public Product GetProduct(string name)
+		{
+			_allProducts.TryGetValue(name, out var product);
+			return product;
+		}
+
+		public List<Product> Seach(string term, StringComparison comparisonType)
+		{
+			return _allProducts
+				.Where(x => x.Value.Name.Contains(term, comparisonType))
+				.Select(x => x.Value)
+				.ToList();
+		}
+	}
+}
