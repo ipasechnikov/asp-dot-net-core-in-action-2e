d8cefcf726acce4465427327d6611cdcedafb05c
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-14 00:57:22 +0900

    Chapter 06: Start working on G_CurrencyConverter solution

---
+++ Chapter06/G_CurrencyConverter/G_CurrencyConverter/Pages/Currency/Convert.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace G_CurrencyConverter.Pages.Currency
+{
+    public class ConvertModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Chapter06/G_CurrencyConverter/G_CurrencyConverter/Pages/Currency/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace G_CurrencyConverter.Pages.Currency
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
