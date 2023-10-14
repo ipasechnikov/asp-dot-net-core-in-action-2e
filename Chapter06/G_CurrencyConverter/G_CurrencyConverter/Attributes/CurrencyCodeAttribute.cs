4a6fcc7a84fc806993b57eb0f5c43141a6b4a773
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-15 01:43:16 +0900

    Chapter 06: Finish G_CurrencyConverter solution

---
+++ Chapter06/G_CurrencyConverter/G_CurrencyConverter/Attributes/CurrencyCodeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace G_CurrencyConverter.Attributes
+{
+	public class CurrencyCodeAttribute : ValidationAttribute
+	{
+		private readonly string[] _allowedCodes;
+
+		public CurrencyCodeAttribute(params string[] allowedCodes)
+		{
+			_allowedCodes = allowedCodes;
+		}
+
+		protected override ValidationResult IsValid(
+			object value,
+			ValidationContext validationContext
+		)
+		{
+			if (value is not string code || !_allowedCodes.Contains(code))
+			{
+				return new ValidationResult("Not a valid currency code");
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
--- Chapter06/G_CurrencyConverter/G_CurrencyConverter/Pages/Currency/Convert.cshtml.cs
+++ Chapter06/G_CurrencyConverter/G_CurrencyConverter/Pages/Currency/Convert.cshtml.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+
+using G_CurrencyConverter.Attributes;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,38 @@ namespace G_CurrencyConverter.Pages.Currency
 {
     public class ConvertModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("Success");
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(3, MinimumLength = 3)]
+            [CurrencyCode("GBP", "USD", "CAD", "EUR")]
+            public string CurrencyFrom { get; set; }
+
+            [Required]
+            [StringLength(3, MinimumLength = 3)]
+            [CurrencyCode("GBP", "USD", "CAD", "EUR")]
+            public string CurrencyTo { get; set; }
+
+            [Required]
+            [Range(1, 1000)]
+            public decimal Quantity { get; set; }
         }
     }
 }
