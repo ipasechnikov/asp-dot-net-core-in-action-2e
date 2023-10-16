d4b411c3d63e987dec0c74fb3f3443a42c808b30
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-17 00:16:36 +0900

    Chapter 06: Add models and services to H_RazorPageFormLayout solution

---
+++ Chapter06/H_RazorPageFormLayout/H_RazorPageFormLayout/Services/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+using H_RazorPageFormLayout.Models;
+
+namespace H_RazorPageFormLayout.Services
+{
+	public class ProductService
+	{
+		public readonly Dictionary<int, ProductDetails> _products = new Dictionary<int, ProductDetails>
+		{
+			{1, new ProductDetails("Apple iPad", 200, 50) },
+			{2, new ProductDetails("Surface", 2200, 10) },
+			{3, new ProductDetails("XPS 15", 1600, 3) },
+		};
+
+		public ProductDetails GetProduct(int productId)
+		{
+			if (_products.TryGetValue(productId, out ProductDetails product))
+			{
+				return product; 
+			}
+			return null;
+		}
+
+		public void UpdateProduct(int productId, string newName, decimal newSellPrice)
+		{
+			ProductDetails product = GetProduct(productId);
+			if (product is null)
+			{
+				return;
+			}
+
+			product.ProductName = newName;
+			product.SellPrice = newSellPrice;
+		}
+	}
+}
