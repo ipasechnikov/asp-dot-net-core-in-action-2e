6bdf7a53084e94e6583a9d676f7c7f2d9f6ff9c3
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-18 01:03:34 +0900

    Chapter 06: Update Index page of H_RazorPageFormLayout solution

--- Chapter06/H_RazorPageFormLayout/H_RazorPageFormLayout/Pages/Index.cshtml.cs
+++ Chapter06/H_RazorPageFormLayout/H_RazorPageFormLayout/Pages/Index.cshtml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using H_RazorPageFormLayout.Models;
+using H_RazorPageFormLayout.Services;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,16 +14,18 @@ namespace H_RazorPageFormLayout.Pages
 {
 	public class IndexModel : PageModel
 	{
-		private readonly ILogger<IndexModel> _logger;
+		private readonly ProductService _productService;
 
-		public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+		public Dictionary<int, ProductDetails> Products { get; private set; }
+
+        public void OnGet()
 		{
-			_logger = logger;
-		}
-
-		public void OnGet()
-		{
-
+			Products = _productService._products;
 		}
 	}
 }
