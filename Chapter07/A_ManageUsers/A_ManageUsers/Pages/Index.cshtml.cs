20f42419b9390ec8e760f682efdadda71b36b350
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-22 23:22:36 +0900

    Chapter 07: Implement Index page and finish A_ManageUsers solution

--- Chapter07/A_ManageUsers/A_ManageUsers/Pages/Index.cshtml.cs
+++ Chapter07/A_ManageUsers/A_ManageUsers/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,16 +12,44 @@ namespace A_ManageUsers.Pages
 {
 	public class IndexModel : PageModel
 	{
-		private readonly ILogger<IndexModel> _logger;
+		/// <summary>
+		/// WARNING: For demo only, not thread safe, you would store these values in a database etc
+		/// </summary>
+		private static readonly List<string> _users = new List<string>
+		{
+			"Bart",
+			"Jimmy",
+			"Robbie",
+		};
+
+		[BindProperty, Required]
+		public string NewUser { get; set; }
+
+		public List<string> ExistingUsers { get; set; }
 
 		public IndexModel(ILogger<IndexModel> logger)
 		{
-			_logger = logger;
+			ExistingUsers = _users;
 		}
 
-		public void OnGet()
+		public IActionResult OnPost()
 		{
+			ExistingUsers = _users;
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
+			if (_users.Contains(NewUser))
+			{
+				// Note, you would typically extract this to a validation attribute
+				// But I do it here as the users list is hard coded above
+				ModelState.AddModelError(nameof(NewUser), "This user already exists!");
+				return Page();
+			}
+
+			_users.Insert(0, NewUser);
+			return RedirectToPage();
 		}
 	}
 }
