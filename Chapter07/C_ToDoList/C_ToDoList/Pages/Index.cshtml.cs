73c33122527e9cf9257d1e3ed78e45e210dc1b2e
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-27 00:03:22 +0900

    Chapter 07: Implement Index page for C_ToDoList solution

--- Chapter07/C_ToDoList/C_ToDoList/Pages/Index.cshtml.cs
+++ Chapter07/C_ToDoList/C_ToDoList/Pages/Index.cshtml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using C_ToDoList.Models;
+using C_ToDoList.Services;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,16 +14,18 @@ namespace C_ToDoList.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ToDoService _toDoService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ToDoService toDoService)
         {
-            _logger = logger;
+            _toDoService = toDoService;
         }
+
+        public IEnumerable<ToDoItem> Items { get; set; }
 
         public void OnGet()
         {
-
+            Items = _toDoService.AllItems;
         }
     }
 }
