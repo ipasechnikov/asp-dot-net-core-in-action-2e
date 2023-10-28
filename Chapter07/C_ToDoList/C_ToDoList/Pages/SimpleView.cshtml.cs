d807b6088800ac59ab5d385f4c0e4274989eebc8
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-10-28 15:29:16 +0900

    Chapter 07: Add SimpleView page to C_ToDoList solution

---
+++ Chapter07/C_ToDoList/C_ToDoList/Pages/SimpleView.cshtml.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+
+using C_ToDoList.Models;
+using C_ToDoList.Services;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C_ToDoList.Pages
+{
+    public class SimpleViewModel : PageModel
+    {
+        private readonly ToDoService _toDoService;
+
+        public SimpleViewModel(ToDoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+        public ToDoItem ToDo { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            ToDo = _toDoService.AllItems.FirstOrDefault(x => x.Id == id);
+            if (ToDo == null)
+            {
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
