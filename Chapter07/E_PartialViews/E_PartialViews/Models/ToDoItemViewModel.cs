165057e0bb1db65c1f684b1ffa32fb8b47bdf818
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-11-02 00:08:40 +0900

    Chapter 07: Add Models and Services to E_PartialViews solution

---
+++ Chapter07/E_PartialViews/E_PartialViews/Models/ToDoItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace E_PartialViews.Models
+{
+    public class ToDoItemViewModel
+    {
+        public ToDoItemViewModel(int id, string title, params string[] tasks)
+        {
+            Id = id;
+            Title = title;
+            Tasks = new List<string>(tasks);
+        }
+
+        public int Id { get; }
+        public string Title { get; }
+        public bool IsCompleted => Tasks.Count == 0;
+        public List<string> Tasks { get; }
+    }
+}
---
+++ Chapter07/E_PartialViews/E_PartialViews/Services/TaskService.cs
@@ -0,0 +1,18 @@
+﻿using E_PartialViews.Models;
+using System.Collections.Generic;
+
+namespace E_PartialViews.Services
+{
+    public class TaskService
+    {
+        //This list would be loaded from a database or file etc
+        public static List<ToDoItemViewModel> AllTasks { get; } = new List<ToDoItemViewModel>
+        {
+            new ToDoItemViewModel(1, "Shopping List", "Buy milk", "Buy eggs", "Buy bread"),
+            new ToDoItemViewModel(2, "Agenda", "Pick up kids", "Take kids to school"),
+            new ToDoItemViewModel(4, "Car stuff", "Get fuel", "Check oil", "Check Tyre pressure"),
+            new ToDoItemViewModel(4, "Problems"),
+            new ToDoItemViewModel(5, "Writing tasks","Write blog post", "Edit book chapter"),
+        };
+    }
+}
