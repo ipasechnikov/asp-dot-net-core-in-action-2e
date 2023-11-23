6590dd91fafe14eab817522ab5b4c50880b51f5c
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-11-24 01:28:54 +0900

    Chapter 08: Start working on Demo page for B_TagHelpers solution

---
+++ Chapter08/B_TagHelpers/B_TagHelpers/Pages/Demo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace B_TagHelpers.Pages
+{
+    public class DemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
