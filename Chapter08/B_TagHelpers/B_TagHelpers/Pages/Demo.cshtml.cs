2af7005476d341d76f4e71f652671213410bfd56
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-12-04 02:15:21 +0900

    Chapter 08: Finish B_TagHelpers solution

--- Chapter08/B_TagHelpers/B_TagHelpers/Pages/Demo.cshtml.cs
+++ Chapter08/B_TagHelpers/B_TagHelpers/Pages/Demo.cshtml.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,10 @@ namespace B_TagHelpers.Pages
 {
     public class DemoModel : PageModel
     {
+        public int Int { get; set; }
+        public bool Boolean { get; set; }
+        public DateTime? DateTime { get; set; }
+
         public void OnGet()
         {
         }
