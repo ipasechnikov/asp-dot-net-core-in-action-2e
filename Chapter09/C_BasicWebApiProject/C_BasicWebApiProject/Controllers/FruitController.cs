5e6de3559a3a8d9a3d26de2c6e9e77ad877dc187
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-12-19 00:40:27 +0900

    Chapter 09: Implement C_BasicWebApiProject solution

---
+++ Chapter09/C_BasicWebApiProject/C_BasicWebApiProject/Controllers/FruitController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C_BasicWebApiProject.Controllers
+{
+    [ApiController]
+    public class FruitController : Controller
+    {
+        private readonly List<string> _fruit = new()
+        {
+            "Pear",
+            "Lemon",
+            "Peach"
+        };
+
+        [HttpGet("fruit")]
+        public IEnumerable<string> Index()
+        {
+            return _fruit;
+        }
+
+        [HttpGet("fruit/{id}")]
+        public ActionResult<string> View(int id)
+        {
+            if (id >= 0 && id <= _fruit.Count)
+            {
+                return _fruit[id];
+            }
+
+            return NotFound();
+        }
+    }
+}
