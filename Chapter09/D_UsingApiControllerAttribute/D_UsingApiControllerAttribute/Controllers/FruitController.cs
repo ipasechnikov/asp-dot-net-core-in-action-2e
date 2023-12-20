d969b9bcc9214f5b30048365c9d6449e2a7598f0
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-12-21 00:12:00 +0900

    Chapter 09: Add FruitController to D_UsingApiControllerAttribute solution

---
+++ Chapter09/D_UsingApiControllerAttribute/D_UsingApiControllerAttribute/Controllers/FruitController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using D_UsingApiControllerAttribute.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D_UsingApiControllerAttribute.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FruitController : Controller
+    {
+        List<string> _fruit = new List<string>
+        {
+            "Pear", "Lemon", "Peach"
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_fruit);
+        }
+
+        [HttpPost]
+        public ActionResult Update(UpdateModel model)
+        {
+            if (model.Id < 0 || model.Id > _fruit.Count)
+            {
+                return NotFound();
+            }
+
+            _fruit[model.Id] = model.Name;
+            return Ok();
+        }
+    }
+}
---
+++ Chapter09/D_UsingApiControllerAttribute/D_UsingApiControllerAttribute/Models/UpdateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D_UsingApiControllerAttribute.Models
+{
+    public class UpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
