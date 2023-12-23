aaa24367be112aaa574556987d0ccb6e2fd2444b
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-12-24 02:28:22 +0900

    Chapter 09: Add ValuesController to E_ProblemDetailsExample solution

---
+++ Chapter09/E_ProblemDetailsExample/E_ProblemDetailsExample/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_ProblemDetailsExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet("{myValue?}")]
+        public IActionResult Get([Required] string myValue)
+        {
+            // Never called, due to automatic invalid response generation
+            return Ok(myValue);
+        }
+    }
+}
