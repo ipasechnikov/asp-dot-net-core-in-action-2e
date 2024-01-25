dea287ba59a51f8beb309d8851b86f238e264b54
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2024-01-25 22:40:41 +0900

    Chapter 09: Add F_CarsWebApi solution

---
+++ Chapter09/F_CarsWebApi/F_CarsWebApi/Controllers/CarsController.cs
@@ -0,0 +1,71 @@
+﻿using F_CarsWebApi.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F_CarsWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : Controller
+    {
+        /// <summary>
+        /// This represents the global application model that would
+        /// normally be stored in a database etc
+        /// </summary>
+        private static List<Car> Cars = new List<Car> { };
+
+        string _carsAsXml = "<cars><car>Nissan Micra</car><car>FordFocus</car></cars>";
+
+        [HttpGet("start")]
+        [HttpGet("ignition")]
+        [HttpGet("/start-car")]
+        public IEnumerable<string> ListCars()
+        {
+            return new string[]
+            {
+                "Nissan Micra",
+                "FordFocus",
+            };
+        }
+
+        [HttpGet("null")]
+        public IActionResult Null()
+        {
+            return Ok(null);
+        }
+
+        [HttpGet("content")]
+        public string ListCarsAsContent()
+        {
+            return _carsAsXml;
+        }
+
+        [HttpGet("xml")]
+        public IActionResult ListCarsAsXml()
+        {
+            return Content(_carsAsXml, "text/xml");
+        }
+
+        [HttpGet("json")]
+        public IActionResult ListCarsAsJson()
+        {
+            return Json(new string[]
+            {
+                "Nissan Micra",
+                "FordFocus",
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Add(Car car)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Cars.Add(car);
+            return Ok();
+        }
+    }
+}
---
+++ Chapter09/F_CarsWebApi/F_CarsWebApi/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F_CarsWebApi.Models
+{
+    public class Car
+    {
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+    }
+}
